39655a2cee6e2c860e0f1e48364a5930a51ec25d
hfccr <hfccr@outlook.com>
2023-02-05 23:39:19 +0530

    Add token

---
+++ contracts/spacetimeToken/SpacetimeToken.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.17;
+
+import "@openzeppelin/contracts/token/ERC20/presets/ERC20PresetMinterPauser.sol";
+
+contract SpacetimeToken is ERC20PresetMinterPauser {
+    constructor() ERC20PresetMinterPauser("Spacetime", "SPT") {}
+}
