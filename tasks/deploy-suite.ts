14b21c498b3738dd4897295368702677fb9043a7
hfccr <hfccr@outlook.com>
2023-02-05 23:41:46 +0530

    Add deploy suite

---
+++ tasks/deploy-suite.ts
@@ -0,0 +1,81 @@
+import "@nomiclabs/hardhat-etherscan"
+import "@nomiclabs/hardhat-waffle"
+import { BigNumber, Contract } from "ethers"
+import { task, types } from "hardhat/config"
+import "hardhat-deploy"
+import "hardhat-deploy-ethers"
+import { DeployOptions } from "hardhat-deploy/dist/types"
+import {
+    SpacetimeToken,
+    SpacetimeDAO
+} from "../typechain-types";
+
+task(`deploy-suite`, `Deploy entire suite of contracts`).setAction(async (taskArgs, hre) => {
+    const { ethers, run, deployments } = hre
+    const { deploy: deployContract } = deployments
+
+    console.log("Starting")
+    const provider = new ethers.providers.JsonRpcProvider(
+        "https://api.hyperspace.node.glif.io/rpc/v1",
+        3141
+    )
+    const deployer = new hre.ethers.Wallet(process.env.PRIVATE_KEY as string, provider)
+
+    const [signer] = await ethers.getSigners()
+
+    console.log("Awaiting provider ready")
+    await provider.ready
+
+    console.log("Awaiting fee data")
+
+    let maxPriorityFee: BigNumber | null = null
+    let attempt = 0
+    while (maxPriorityFee == null) {
+        try {
+            maxPriorityFee = (await provider.getFeeData()).maxPriorityFeePerGas
+        } catch (e) {
+            attempt++
+            if (attempt > 100) {
+                break
+            }
+        }
+    }
+
+    const deploy = async (name: string, args: any[] = []) => {
+        const deployResult = await deployContract(name, {
+            from: deployer.address,
+            args,
+            // since it's difficult to estimate the gas before f4 address is launched, it's safer to manually set
+            // a large gasLimit. This should be addressed in the following releases.
+            gasLimit: 1000000000, // BlockGasLimit / 10
+            // since Ethereum's legacy transaction format is not supported on FVM, we need to specify
+            // maxPriorityFeePerGas to instruct hardhat to use EIP-1559 tx format
+            maxPriorityFeePerGas: maxPriorityFee ?? undefined,
+            log: true,
+        })
+        console.log(`${name} at address ${deployResult.address}`)
+
+        return new hre.ethers.Contract(deployResult.address, deployResult.abi, signer)
+    }
+
+    console.log("Awaiting deploys")
+
+    const spacetimeToken = (await deploy("SpacetimeToken")) as SpacetimeToken
+    const spacetimeDao = (await deploy("SpacetimeDAO", [spacetimeToken.address])) as SpacetimeDAO
+    console.log('Granting minter role');
+    await spacetimeToken.grantRole(ethers.utils.keccak256(ethers.utils.toUtf8Bytes("MINTER_ROLE")), spacetimeDao.address, {
+        gasLimit: 10000000,
+        maxPriorityFeePerGas: maxPriorityFee?.toString(),
+    });
+    console.log('Granting pauser role');
+    await spacetimeToken.grantRole(ethers.utils.keccak256(ethers.utils.toUtf8Bytes("PAUSER_ROLE")), spacetimeDao.address, {
+        gasLimit: 10000000,
+        maxPriorityFeePerGas: maxPriorityFee?.toString(),
+    });
+    console.info({
+        spacetimeDao: spacetimeDao.address,
+        spacetimeToken: spacetimeToken.address,
+    })
+})
+
+export default {}
